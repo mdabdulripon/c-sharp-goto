bf3daa4077ffb204435573628572edd7dee9bf92
Abdul Ripon <mdabdulripon@gmail.com>
2020-05-14 10:22:10 -0500

    practice(more methods of queue)

--- Generic/Queue.cs
+++ Generic/Queue.cs
@@ -8,29 +8,29 @@ namespace Queue
         static void Main(string[] args)
         {
             // Queue => First in first out. For example people are standing in a line.
+            // The first person in the line get the serve first and get out first.
             var line = new Queue<Employee>();
             line.Enqueue(new Employee { name = "Ripon"});
             line.Enqueue(new Employee { name = "Shama" });
             line.Enqueue(new Employee { name = "Mun" });
             line.Enqueue(new Employee { name = "Sadia" });
 
+            Console.WriteLine("------------------------- Reading each queue members -------------------------");
             foreach (var emp in line)
             {
                 Console.WriteLine(emp.name); // Ripon, Shama, Mun, Sadia
             }
-            
-            Console.WriteLine("=================================");
-            Console.WriteLine(line.Count);
+
+            Console.WriteLine("------------------------- Grt the first queue and length -------------------------");
+            Console.WriteLine(line.Count); // 4
             var pickOne = line.Peek();
-            Console.WriteLine(pickOne.name);
-            Console.WriteLine("=================================");
+            Console.WriteLine(pickOne.name); // Ripon
 
-
-
+            Console.WriteLine("------------------------- Removing each queue members -------------------------");
             while (line.Count > 0)
             {
                 var emp = line.Dequeue();
-                Console.WriteLine(emp.name);
+                Console.WriteLine(emp.name); // Ripon, Shama, Mun, Sadia
             }
         }
     }
